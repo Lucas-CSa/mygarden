6beb3ee990bd7d0d84e31be79e9e2aa1fd853c0b
Lucas-CSa <lucas.cisa19@gmail.com>
2019-10-29 15:29:39 -0300

    Project Starting Point

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,19 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using mygarden.Models;
 
 namespace mygarden.Controllers
 {
-    public class HomeController : Controller
+    public class ProfileController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
-
     }
 }
---
+++ Models/ProdutorModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mygarden.Models
+{
+    public class ProdutorModel
+    {
+        public int ProdId { get; set; }
+
+        public string ProdNom { get; set; }
+
+        public string ProdEnd { get; set; }
+
+        public string ProdBairro { get; set; }
+
+        public string ProdNum { get; set; }
+
+        public string ProTel { get; set; }
+
+        public string ProDesc { get; set; }
+    }
+}
